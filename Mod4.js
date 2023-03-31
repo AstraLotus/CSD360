81f7cde6d4fa0c463f0289aff11ebcbef69a2b0c
AstraLotus <116016761+AstraLotus@users.noreply.github.com>
2023-03-31 17:46:46 -0500

    Update Mod4.js

--- Mod4.js
+++ Mod4.js
@@ -1,8 +1,12 @@
+// Author: Zoey Naizer
+// Assignment: Module 4
+// Date: 3-31-23
+
 var i = 842;
-document.write(i + i % 3 / 7 - 2 + 7 ** 3);
-document.write(((i + i % 3) / 7 - 2 + 7 ** 3).toFixed(14));
-document.write(((i + i % 3) / 7 - 2 + 7 ** 3 + 2.115).toFixed(1));
-document.write((i + (i % 3 / 7) - 2 + 7 ** 3).toFixed(13));
-document.write(((i + i % 3) / (7 - 2 + 7 ** 3)).toFixed(14));
-document.write(((i + i % 3) / 7 - 2 + 7 ** (3 / 2)).toFixed(1));
-document.write((i + i % (3 / 7) - 2 + 7 ** 3).toFixed(2));
+document.write((i + (i % 3) / 7 - 2 + 7 ** 3).toFixed(14));
+document.write(((i + i % 3) / 7 - 2 + 7 ** 3).toFixed(1));
+document.write((i + (i % 3) / 7 - 2 + 7 ** 3 + 0.00115740740741).toFixed(13));
+document.write((i + (i % 3) / 7 - 2 + 7 ** 3).toFixed(14));
+document.write(((i + i % 3) / (7 - 2 + 7 ** 3)).toFixed(1));
+document.write(((i + i % 3) / 7 - 2 + 7 ** (3 / 2)).toFixed(2));
+document.write((i + (i % (3 / 7)) - 2 + 7 ** 3).toFixed(2));
